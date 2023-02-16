35acfc5a0eac234597d8c76ec0d29f03c61209a8
“Anne <anne.nkone@gmail.com>
2023-02-16 12:26:13 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with printf function
- * Return: 0
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-Return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
