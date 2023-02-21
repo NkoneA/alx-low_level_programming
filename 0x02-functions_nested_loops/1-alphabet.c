f50bb813e44f4d665ad583f7c75363da27d5e0e4
“Anne <anne.nkone@gmail.com>
2023-02-21 14:47:18 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ *print_alphabet - prints alphabets in lowercase
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-	int n;
+	char ch;
 
-	for (n = 'a'; n <= 'z'; n++)
-	{
-		_putchar(n);
-	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
 	_putchar('\n');
 }
-
