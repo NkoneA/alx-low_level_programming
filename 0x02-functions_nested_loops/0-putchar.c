50c0974b954271364d72924c9682fe7cdc9ca240
“Anne <anne.nkone@gmail.com>
2023-02-21 13:04:24 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include "-putchar.h"
+#include "main.h"
 /**
- * main - Entry point
+ * main - main function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
  *
- * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char ptc = "-putchar";
-
-	while (ptc)
-	{
-		_putchar(ptc);
-		ptc++;
-	}
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
