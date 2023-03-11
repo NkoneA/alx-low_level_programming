d8e9729a224f993c5ed850ac5759e0b906db41ff
“Anne <anne.nkone@gmail.com>
2023-03-10 16:20:49 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,12 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]);
-int main(int argc, char *argv[] __attribute__((unused)));
+int _atoi(char *s);
+int _atoi(char *s);
 int main(int argc, char *argv[]);
-int _atoi(char *s);
-int _atoi(char *s);
-int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
